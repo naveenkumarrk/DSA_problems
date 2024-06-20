f61ae6b456e0b0820ffa53ebb7a3ab861d72502a
naveenrk7 <111naveenkumarrk@gmail.com>
2024-06-20 22:56:04 +0530

    f

--- Leetcode problems/1552_magnetic_field.py
+++ Leetcode problems/1552_magnetic_field.py
@@ -22,3 +22,33 @@ class Solution:
         r = mid - 1
 
     return l
+  
+
+
+
+
+#   class Solution:
+
+#     def maxDistance(self, position: List[int], m: int) -> int:
+
+#         def check(minmax):
+#             rem = m - 1
+#             lim = position[0] + minmax
+#             for p in position[1:]:
+#                 if p >= lim:
+#                     rem -= 1
+#                     lim = p + minmax
+#             return rem <= 0
+
+#         position.sort()
+#         if m == 2:
+#             return position[-1] - position[0]
+#         low = 1
+#         high = (position[-1] - position[0]) // (m - 1)
+#         while low < high:
+#             mid = (low + high + 1) // 2
+#             if check(mid):
+#                 low = mid
+#             else:
+#                 high = mid -1
+#         return low        
